92402d0d3dfa3d2e75783c8b03f0c0c8c1dca92b
ThuyHien261195 <thuyhien261195@gmail.com>
2017-10-02 19:03:57 +0700

    Update code to load data from server

--- app/src/main/java/com/hasbrain/areyouandroiddev/activity/PostListActivity.java
+++ app/src/main/java/com/hasbrain/areyouandroiddev/activity/PostListActivity.java
@@ -1,10 +1,14 @@
-package com.hasbrain.areyouandroiddev;
+package com.hasbrain.areyouandroiddev.activity;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import com.hasbrain.areyouandroiddev.DownloadTask;
+import com.hasbrain.areyouandroiddev.R;
+import com.hasbrain.areyouandroiddev.adapter.RedditPostAdapter;
 import com.hasbrain.areyouandroiddev.datastore.FeedDataStore;
 import com.hasbrain.areyouandroiddev.datastore.FileBasedFeedDataStore;
+import com.hasbrain.areyouandroiddev.datastore.NetworkBasedFeedDataStore;
 import com.hasbrain.areyouandroiddev.model.RedditPost;
 import com.hasbrain.areyouandroiddev.model.RedditPostConverter;
 
@@ -12,7 +16,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.MotionEvent;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,9 +24,11 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class PostListActivity extends AppCompatActivity {
+public class PostListActivity extends AppCompatActivity
+        implements FeedDataStore.OnRedditPostsRetrievedListener {
 
     public static final String DATA_JSON_FILE_NAME = "data.json";
+    public static final String URL_DATA_JSON = "https://www.reddit.com/r/androiddev/new.json";
     private FeedDataStore feedDataStore;
 
     @BindView(R.id.recycler_view_reddit_post)
@@ -35,49 +40,56 @@ public class PostListActivity extends AppCompatActivity {
         setContentView(getLayoutResource());
 
         ButterKnife.bind(this);
-        initViews();
+        initDataByDownloading();
     }
 
-    public void initViews() {
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.registerTypeAdapter(RedditPost.class, new RedditPostConverter());
-        Gson gson = gsonBuilder.create();
-        InputStream is = null;
-        try {
-            is = getAssets().open(DATA_JSON_FILE_NAME);
-            feedDataStore = new FileBasedFeedDataStore(gson, is);
-            feedDataStore.getPostList(new FeedDataStore.OnRedditPostsRetrievedListener() {
-                @Override
-                public void onRedditPostsRetrieved(List<RedditPost> postList, Exception ex) {
-                    displayPostList(postList);
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (is != null) {
-                try {
-                    is.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+    public void initDataByDownloading() {
+        DownloadTask downloadTask = new DownloadTask(this);
+        downloadTask.execute();
+    }
+
+    @Override
+    public void onRedditPostsRetrieved(List<RedditPost> postList, Exception ex) {
+        if (postList != null) {
+            displayPostList(postList);
         }
     }
 
     protected void displayPostList(List<RedditPost> postList) {
-        // Add item null is a footer item
-        RedditPost footerPostItem = new RedditPost();
-        footerPostItem.setViewType(ConstantCollection.FOOTER_VIEW);
-        postList.add(footerPostItem);
-
+        RedditPostAdapter redditPostAdapter = new RedditPostAdapter(this, postList);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         recyclerViewRedditPost.setLayoutManager(linearLayoutManager);
-        RedditPostAdapter redditPostAdapter = new RedditPostAdapter(this, postList);
         recyclerViewRedditPost.setAdapter(redditPostAdapter);
     }
 
     protected int getLayoutResource() {
         return R.layout.activity_post_list;
     }
+
+//    public void initViews() {
+//        GsonBuilder gsonBuilder = new GsonBuilder();
+//        gsonBuilder.registerTypeAdapter(RedditPost.class, new RedditPostConverter());
+//        Gson gson = gsonBuilder.create();
+//        InputStream is = null;
+//        try {
+//            is = getAssets().open(DATA_JSON_FILE_NAME);
+//            feedDataStore = new FileBasedFeedDataStore(gson, is);
+//            feedDataStore.getPostList(new FeedDataStore.OnRedditPostsRetrievedListener() {
+//                @Override
+//                public void onRedditPostsRetrieved(List<RedditPost> postList, Exception ex) {
+//                    displayPostList(postList);
+//                }
+//            });
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (is != null) {
+//                try {
+//                    is.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    }
 }
