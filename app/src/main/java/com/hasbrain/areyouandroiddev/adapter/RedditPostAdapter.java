14cf2bc1d723574bc00ccc86758dcf84b2f0a97a
ThuyHien261195 <thuyhien261195@gmail.com>
2017-10-03 15:10:09 +0700

    Update function addLoadingItem, RemoveLoadingItem, updatePostList, getItemCount, and ViewType

--- app/src/main/java/com/hasbrain/areyouandroiddev/adapter/RedditPostAdapter.java
+++ app/src/main/java/com/hasbrain/areyouandroiddev/adapter/RedditPostAdapter.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 import com.hasbrain.areyouandroiddev.FormatStringUtils;
 import com.hasbrain.areyouandroiddev.R;
+import com.hasbrain.areyouandroiddev.activity.PostListActivity;
 import com.hasbrain.areyouandroiddev.model.RedditPost;
 
 import java.util.HashMap;
@@ -25,7 +26,8 @@ import butterknife.ButterKnife;
 
 public class RedditPostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     public static final int CONTENT_VIEW = 0;
-    public static final int FOOTER_VIEW = 1;
+    public static final int LOADING_VIEW = 1;
+    private boolean isLoading = false;
 
     private List<RedditPost> redditPostList;
     private HashMap<String, String> timeTitleList;
@@ -44,9 +46,9 @@ public class RedditPostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
             case CONTENT_VIEW:
                 rowView = inflater.inflate(R.layout.item_card_view_post, parent, false);
                 return new PostViewHolder(rowView, timeTitleList);
-            case FOOTER_VIEW:
-                rowView = inflater.inflate(R.layout.item_footer, parent, false);
-                return new FooterViewHolder(rowView);
+            case LOADING_VIEW:
+                rowView = inflater.inflate(R.layout.item_loading, parent, false);
+                return new LoadingViewHolder(rowView);
             default:
                 break;
         }
@@ -60,7 +62,7 @@ public class RedditPostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
             case CONTENT_VIEW:
                 ((PostViewHolder) holder).bindContentView(redditPostList.get(position));
                 break;
-            case FOOTER_VIEW:
+            case LOADING_VIEW:
                 break;
             default:
                 break;
@@ -69,15 +71,44 @@ public class RedditPostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
 
     @Override
     public int getItemCount() {
-        return redditPostList.size() + 1;
+        int itemCount = redditPostList.size();
+        if (isLoading) {
+            itemCount += 1;
+        }
+        return itemCount;
     }
 
     @Override
     public int getItemViewType(int position) {
         if (position == redditPostList.size()) {
-            return FOOTER_VIEW;
+            return LOADING_VIEW;
         } else {
             return CONTENT_VIEW;
         }
+    }
+
+    public void updatePostListData(int loadingState, List<RedditPost> newPostList) {
+        if (loadingState == PostListActivity.REFRESH && redditPostList != null) {
+            redditPostList.clear();
+        }
+
+        if (redditPostList != null && redditPostList.size() > 0) {
+            redditPostList.addAll(newPostList);
+        } else {
+            redditPostList = newPostList;
+        }
+        notifyDataSetChanged();
+    }
+
+    public void addLoadingItem() {
+        isLoading = true;
+        notifyItemInserted(redditPostList.size());
+        notifyDataSetChanged();
+    }
+
+    public void removeLoadingItem() {
+        isLoading = false;
+        notifyItemRemoved(redditPostList.size());
+        notifyDataSetChanged();
     }
 }
