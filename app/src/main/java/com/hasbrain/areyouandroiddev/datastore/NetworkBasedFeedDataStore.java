9d7a2f908f6967d6ae553836623e5ad2fb2671b5
ThuyHien261195 <thuyhien261195@gmail.com>
2017-10-04 15:09:12 +0700

    Update BaseUrl constant, function getRedditPostList by Retrofit

--- app/src/main/java/com/hasbrain/areyouandroiddev/datastore/NetworkBasedFeedDataStore.java
+++ app/src/main/java/com/hasbrain/areyouandroiddev/datastore/NetworkBasedFeedDataStore.java
@@ -1,13 +1,14 @@
 package com.hasbrain.areyouandroiddev.datastore;
 
-import android.content.res.TypedArray;
 import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import com.hasbrain.areyouandroiddev.ApiEndpointInterface;
 import com.hasbrain.areyouandroiddev.model.RedditPost;
 import com.hasbrain.areyouandroiddev.model.RedditPostConverter;
+import com.hasbrain.areyouandroiddev.model.RedditPostListConverter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -17,12 +18,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Arrays;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by thuyhien on 10/2/17.
@@ -32,31 +36,53 @@ public class NetworkBasedFeedDataStore implements FeedDataStore {
 
     private static final int READ_TIME_OUT = 3000;
     private static final int CONNECT_TIME_OUT = 3000;
-    public static final String URL_DATA_JSON = "https://www.reddit.com/r/androiddev/new.json";
+    public static final String BASE_URL = "https://www.reddit.com/r/androiddev/";
     private static final int LOADING_ITEM_LIMIT = 8;
 
     @Override
     public void getPostList(String topic, String before, String after,
                             OnRedditPostsRetrievedListener onRedditPostsRetrievedListener) {
-        List<RedditPost> redditPostList = null;
         try {
-            String downloadUrl = setBaseUrl(after);
-            redditPostList = downloadFromUrl(downloadUrl);
+            List<RedditPost> redditPostList = downloadFromUrlByRetrofit(after);
+            onRedditPostsRetrievedListener.onRedditPostsRetrieved(redditPostList, null);
         } catch (IOException e) {
             onRedditPostsRetrievedListener.onRedditPostsRetrieved(null, e);
         }
-        onRedditPostsRetrievedListener.onRedditPostsRetrieved(redditPostList, null);
     }
 
-    public String setBaseUrl(String afterId) {
-        String downloadUrl = URL_DATA_JSON + "?limit=" + LOADING_ITEM_LIMIT;
+    private List<RedditPost> downloadFromUrlByRetrofit(String afterId) throws IOException {
+        List<RedditPost> redditPostList = null;
+        Type type = new TypeToken<List<RedditPost>>() {
+        }.getType();
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(type, new RedditPostListConverter());
+        Gson gson = gsonBuilder.create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        ApiEndpointInterface apiService = retrofit.create(ApiEndpointInterface.class);
+
+        Call<List<RedditPost>> call = apiService.getRedditPostList(LOADING_ITEM_LIMIT, afterId);
+
+        Response<List<RedditPost>> redditPostListResponse = call.execute();
+        if (redditPostListResponse.isSuccessful()) {
+            redditPostList = redditPostListResponse.body();
+        }
+        return redditPostList;
+    }
+
+    private String setBaseUrl(String afterId) {
+        String downloadUrl = BASE_URL + "new.json" + "?limit=" + LOADING_ITEM_LIMIT;
         if (!afterId.equals("")) {
             downloadUrl += "&after=" + afterId;
         }
         return downloadUrl;
     }
 
-    public List<RedditPost> downloadFromUrl(String downloadUrl) throws IOException {
+    private List<RedditPost> downloadFromUrl(String downloadUrl) throws IOException {
         InputStream inputStream = null;
         HttpURLConnection connection = null;
         List<RedditPost> redditPostList = null;
